234693ea41b16e78197cfe718a0a2454263d5440
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-01-13 02:26:02 +0100

    fixes

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.use(function (req, res, next) {
 // CORS Policy things
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
