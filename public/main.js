74f626ff027227629111746ff5a8298532f444f9
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-01-13 03:11:56 +0100

    Changes Website mixelblocks.de

--- public/main.js
+++ public/main.js
@@ -1,1 +1,30 @@
-console.log('Hier entsteht eine neue Website.');
+var loads = true;
+
+window.addEventListener('load', function theseFunc() {
+    fetch('https://mcapi.us/server/status?ip=proxy.mixelblocks.de&port=25577')
+        .then((res) => res.json())
+        .then((data) => {
+            document.querySelector('.players').innerHTML = data.players.now + '/' + data.players.max;
+            loads = false;
+        });
+
+    document.querySelector('.updateServer').addEventListener('click', function copyIP() {
+        if (!loads) {
+            loads = true;
+            document.querySelector('.players').innerHTML = 'lädt...';
+
+            fetch('https://mcapi.us/server/status?ip=proxy.mixelblocks.de&port=25577')
+                .then((res) => res.json())
+                .then((data) => {
+                    setTimeout(() => {
+                        document.querySelector('.players').innerHTML = data.players.now + '/' + data.players.max;
+                        loads = false;
+                    }, 650);
+                });
+        }
+    });
+    document.querySelector('.copyIP').addEventListener('click', function copyIP() {
+        var clipboard = new ClipboardJS('.copyIP');
+        alertify.success('SERVER IP KOPIERT!');
+    });
+});
